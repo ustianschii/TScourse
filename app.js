76f01131810d0db952d9791da11342ec7fbca376
Cristian <ustianschii@gmail.com>
2024-04-04 01:18:35 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+const skills = ["Dev", "DevOps", "Testing"];
+// for (const skill of skills) {
+//   console.log(skill.toLowerCase());
+// }
+const res = skills
+    .filter((s) => s !== "DevOps")
+    .map((s) => s + ".")
+    .reduce((a, b) => a + b);
+console.log(res);
---
+++ app.ts
@@ -0,0 +1,12 @@
+const skills: string[] = ["Dev", "DevOps", "Testing"];
+
+// for (const skill of skills) {
+//   console.log(skill.toLowerCase());
+// }
+
+const res = skills
+  .filter((s: string) => s !== "DevOps")
+  .map((s) => s + ".")
+  .reduce((a, b) => a + b);
+
+console.log(res);
